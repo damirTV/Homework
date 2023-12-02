0fd53ccf8ee0fc7372defcc41b5bd2319bbbcb88
dash01 <damir@shakirov.tv>
2023-12-02 13:54:01 +0300

    Заготовка для ДЗ№2

---
+++ src/Homework2.java
@@ -0,0 +1,10 @@
+
+public class Homework2 {
+    public static void main(String[] args) {
+        ex1(); // Выполняем задание №1
+    }
+
+    public static void ex1() {
+        System.out.println("Задание №1");
+        }
+    }
