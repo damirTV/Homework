98fe267050c64bce24cf370b1f9d0740d5c5a6fe
dash01 <damir@shakirov.tv>
2023-12-14 22:48:07 +0300

    переложил классы по папкам

--- src/third/ex5/City.java
+++ src/third/ex5/City.java
@@ -5,10 +5,13 @@ import java.util.Arrays;
 public final class City {
     private final String name;
     private final House[] houses;
+    private final House[] copyhouses;
 
     public City(String name, House[] houses) {
         this.name = name;
         this.houses = houses;
+        this.copyhouses = houses;
+        System.arraycopy(houses, 0, copyhouses, 0, houses.length);
     }
 
     public String getName() {
@@ -16,11 +19,11 @@ public final class City {
     }
 
     public House[] getHouses() {
-        return houses;
+        return copyhouses;
     }
 
     @Override
     public String toString() {
-        return getName() + " " + Arrays.toString(getHouses());
+        return getName() + " " + Arrays.toString(houses);
     }
 }
